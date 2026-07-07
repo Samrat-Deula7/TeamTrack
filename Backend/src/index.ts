a43c0007d470ce630cd2b143bb80ad263d06b5f3
Samrat-Deula7 <samratdeula461@gmail.com>
2026-07-07 15:26:24 +0545

    starting work in the backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,23 +1,21 @@
 import express, { Application, Request, Response } from "express";
 import tasksRouter from "./routes/Task";
-import teamtaskRouter from "./routes/TeamTask"
+import teamtaskRouter from "./routes/TeamTask";
 import cors from "cors";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 
-
-app.use(
-  cors(),
-);
-
-app.use(express.json())
+app.use(express.json());
 
 // app.use(cors())
 app.use("/api/tasks", tasksRouter);
 app.use("/api/teamtasks", teamtaskRouter);
 
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
-
-export default app; 
+export default app;
