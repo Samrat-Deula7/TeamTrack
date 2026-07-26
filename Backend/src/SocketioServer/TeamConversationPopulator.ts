deadcd846521801084b17877828920cb115ea166
Samrat-Deula7 <samratdeula461@gmail.com>
2026-07-26 11:14:47 +0545

    Working on conversation populate api

--- Backend/src/SocketioServer/TeamConversationPopulator.ts
+++ Backend/src/SocketioServer/TeamConversationPopulator.ts
@@ -5,9 +5,25 @@ import { pool } from "../routes/Task";
 const ConversationRouter = express.Router();
 
 ConversationRouter.post("/talk", async (req: Request, res: Response) => {
-  const token = req.header("FlowTrackAuthtoken");
-  let authData = await SocketUserAuth(token!);
+  try {
+    const token = req.header("FlowTrackAuthtoken");
+    let authData = await SocketUserAuth(token!);
 
-  res.status(200).json({ success: [authData?.team_id, authData?.user_name] });
+    const { mess } = req.body;
+
+    const insertQuery = `
+      INSERT INTO team_conversation (team_id,conversation,team_member)
+      VALUES ($1,$2,$3);
+    `;
+    const values = [authData?.team_id, mess, authData?.user_name];
+    await pool.query(insertQuery, values);
+
+    res.status(200).json({ success: "conversation populated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: "unable to pupulate conversation.",
+    });
+  }
 });
 export default ConversationRouter;
