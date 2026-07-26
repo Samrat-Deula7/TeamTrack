bb26cf2a91fadf567c8268b82037f3e3ed503c20
Samrat-Deula7 <samratdeula461@gmail.com>
2026-07-26 11:20:30 +0545

    Added check for message

--- Backend/src/SocketioServer/TeamConversationPopulator.ts
+++ Backend/src/SocketioServer/TeamConversationPopulator.ts
@@ -1,10 +1,22 @@
 import SocketUserAuth from "./SocketUserAuth";
 import express, { Request, Response } from "express";
+import { body,validationResult } from "express-validator";
 import { pool } from "../routes/Task";
 
 const ConversationRouter = express.Router();
 
-ConversationRouter.post("/talk", async (req: Request, res: Response) => {
+ConversationRouter.post("/talk",[
+  body(
+    "mess",
+    "Cannot be empty",
+  ).isLength({min:1})
+], async (req: Request, res: Response) => {
+  // The above array will set the restrictions rules and the following code will give error if those rules are broken.
+  const errors = validationResult(req);
+  // If error is empty is false then there is error so the if statement cathes the error.
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     const token = req.header("FlowTrackAuthtoken");
     let authData = await SocketUserAuth(token!);
